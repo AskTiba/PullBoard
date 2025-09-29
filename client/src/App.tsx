6a7eb23d36762930e8dbb2208a5b7f636d9dd12e
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-09-29 19:58:29 +0300

    update: implemented react-router-dom for navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,39 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import OpenPRs from "./components/pages/OpenPRs";
+import ClosedPRs from "./components/pages/ClosedPRs";
 import "./App.css";
+// using react-router-dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 function App() {
+   const router = createBrowserRouter(
+     [
+      {
+        path: "/",
+        element: <><Navbar /><Header /></>
+      },
+      {
+        path: "/home",
+        element: <><Navbar /><Home/></>
+      },
+      {
+        path: "/open-prs",
+        element: <><Navbar /><OpenPRs/></>
+      },
+      {
+        path: "/closed-prs",
+        element: <><Navbar /><ClosedPRs/></>
+      },
+    ]
+  )
   return (
     <>
-      <div className="max-w-screen">
-        <Navbar />
-        <Header />
+      <div>
+        <RouterProvider router={router}/>
+        
       </div>
     </>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import PBLogo from "./brand/PBLogo";
 import DateDisplay from "./ui/DateDisplay";
 import HamburgerComponent from "./Hamburger";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="border-b-[0.5px] border-gray-300 py-2 px-4 ">
       <div className="flex justify-between max-w-7xl mx-auto items-center relative">
         {/* logo and app name*/}
-        <div className="flex items-center gap-2 cursor-pointer">
+       <Link to={"/"}>
+       <div className="flex items-center gap-2 cursor-pointer">
           <PBLogo fill="#fff" width={55} className="" />
           <div className="text-2xl text-gray-600 font-semibold">Pull Board</div>
         </div>
+       </Link> 
 
         {/* Navigation */}
         <div className="block md:hidden">
-        <HamburgerComponent />
+          <HamburgerComponent />
         </div>
         <div className="md:flex items-center gap-2 hidden">
           <DateDisplay
@@ -24,15 +27,21 @@ const Navbar = () => {
           />
 
           {/* Using hamburger-react icons */}
-          <div className="cursor-pointer text-gray-600 text-[16px] hover:bg-gray-100 p-2 hover:rounded-md">
-            Home
-          </div>
-          <div className="cursor-pointer text-gray-600 text-[16px] hover:bg-gray-100 p-2 hover:rounded-md">
-            Open PRs
-          </div>
-          <div className="cursor-pointer text-gray-600 text-[16px] hover:bg-gray-100 p-2 hover:rounded-md">
-            Closed PRs
-          </div>
+          <Link to="/home">
+            <div className="cursor-pointer text-gray-600 text-[16px] hover:bg-gray-100 p-2 hover:rounded-md">
+              Home
+            </div>
+          </Link>
+          <Link to="/open-prs">
+            <div className="cursor-pointer text-gray-600 text-[16px] hover:bg-gray-100 p-2 hover:rounded-md">
+              Open PRs
+            </div>
+          </Link>
+          <Link to="/closed-prs">
+            <div className="cursor-pointer text-gray-600 text-[16px] hover:bg-gray-100 p-2 hover:rounded-md">
+              Closed PRs
+            </div>
+          </Link>
         </div>
       </div>
     </div>
