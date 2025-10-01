697b19f17da22231b624a7b5566a8c3d2f94e3a9
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-10-01 20:47:43 +0300

    update: refined navbar and hero section to align with the latest design

--- client/src/components/layout/Hamburger.tsx
+++ client/src/components/layout/Hamburger.tsx
@@ -6,19 +6,19 @@ const HamburgerComponent = () => {
   const [open, setOpen] = useState(false);
   return (
     <div>
-      <div className="text-gray-600">
-        <Hamburger size={24} toggled={open} toggle={setOpen} />
+      <div className="text-gray-600 relative">
+        <Hamburger size={24} toggled={open} toggle={setOpen}/>
       </div>
       {open && (
         <div>
-          <div className="absolute right-4 top-14 bg-white border-[0.5px] border-gray-400 p-6 flex flex-col gap-2 rounded-sm">
-            <div className="cursor-pointer text-gray-600 text-[16px] hover:text-gray-800">
+          <div className="absolute right-8 top-18 bg-white border-[0.5px] border-gray-400 p-6 flex flex-col gap-2 rounded-sm z-1">
+            <div className="cursor-pointer text-gray-600 text-[18px] hover:text-gray-800">
               Home
             </div>
-            <div className="cursor-pointer text-gray-600 text-[16px] hover:text-gray-800">
+            <div className="cursor-pointer text-gray-600 text-[18px] hover:text-gray-800">
               Open PRs
             </div>
-            <div className="cursor-pointer text-gray-600 text-[16px] hover:text-gray-800">
+            <div className="cursor-pointer text-gray-600 text-[18px] hover:text-gray-800">
               Closed PRs
             </div>
           </div>
